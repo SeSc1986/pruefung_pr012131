46da9fba6d525c08bd38bca52ab7c6d5aaa6cad8
Sebastian <sebastian.schneckenburger@posteo.de>
2021-11-01 20:19:17 +0100

    #aufgabe_02 initialer commit

---
+++ Aufgaben/Aufgabe_02/Aufgabe_02/Aufgabe_02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_02
+{
+    class Program
+    {
+        private static int lastInput = 0;
+        private static List<int> inputNumbers = new List<int>();
+
+        static void Main(string[] args)
+        {
+            List<int> inputNumbers = new List<int>();
+
+            Console.WriteLine("Dieses Programm liest solange Zahlen ein, bis das erste mal eine '0' eingegeben wird.\n" +
+                        "Dann wird die Summe und der Mittelwert der bis dahin eingegeben Zahlen berechnet.");
+            Console.WriteLine("Zahleneingabe >>>");
+
+
+            ZahlenEingabe();
+        
+
+        }
+
+        private static void ZahlenEingabe()
+        {
+            string input = Console.ReadLine();
+
+
+            try
+            {
+                if (Int32.TryParse(input, out lastInput))
+                {
+
+                }
+                else
+                {
+                    throw
+                }
+            }
+            catch (Exception e)
+            {
+     
+                Console.WriteLine("Bitte nur ganze Zahlen eingeben");
+                ZahlenEingabe();
+            }
+
+
+            if (lastInput == 0)
+            {
+
+                string summe = inputNumbers.Sum().ToString();
+                string durchschnitt = inputNumbers.Average().ToString();
+
+                Console.WriteLine("Die Summe der eingegebenen Zahlen ist " + summe + " und der Durchschnitt beträgt " + durchschnitt + ".");
+
+                Console.ReadLine();
+            }
+            else
+            {
+                inputNumbers.Add(lastInput);
+                ZahlenEingabe();
+            }
+       }
+    }
+}
