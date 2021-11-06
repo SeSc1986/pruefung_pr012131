a59146db86978e1042db21a0d14b3c29b5933626
SeSc1986 <sebastian.schneckenburger@posteo.de>
2021-11-06 11:10:45 +0100

    Auslagerung der Logik in eine eigene Lottoklasse #aufgabe_04

---
+++ Aufgaben/Aufgabe_04/Aufgabe_04/Aufgabe_04/Lotto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Aufgabe_04
+{
+   
+
+    class Lotto
+    {
+
+        private static List<int> lottozahlen = new List<int>();
+
+        public List<int> GezogeneZahlen { get; set; } = new List<int>();
+
+        // I may not be used
+        private Lotto() { }
+
+
+        public Lotto(int anzahlDerGezogenenZahlen, int bascet)
+        {
+            InitializeBasket(bascet);
+            GezogeneZahlen = Ziehung(anzahlDerGezogenenZahlen);
+
+        }
+
+
+        private static void InitializeBasket(int anzahlZahlen)
+        {
+            for (int i = 0; i < anzahlZahlen; i++)
+            {
+                lottozahlen.Add(i);
+            }
+        }
+
+
+        public static List<int> Ziehung(int anzahlDerGezogenenZahlen)
+        {
+            List<int> interimswerte = new List<int>();
+
+            for (int i = 0; i < anzahlDerGezogenenZahlen; i++)
+            {
+                interimswerte = Einzelziehung(interimswerte);
+            }
+
+            return interimswerte;
+        }
+
+        private static List<int> Einzelziehung(List<int> interimswerte)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            byte[] box = new byte[4];
+            provider.GetBytes(box);
+            int starkeZufallszahl = BitConverter.ToInt32(box, 0);
+
+            Random zufall = new Random(starkeZufallszahl);
+            int zufallszahl = zufall.Next(1, lottozahlen.Count);
+            interimswerte.Add(lottozahlen[zufallszahl - 1]);
+            lottozahlen.RemoveAt(zufallszahl - 1);
+            return interimswerte;
+        }
+
+    }
+}
--- Aufgaben/Aufgabe_04/Aufgabe_04/Aufgabe_04/Program.cs
+++ Aufgaben/Aufgabe_04/Aufgabe_04/Aufgabe_04/Program.cs
@@ -1,26 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Aufgabe_04
 {
     class Program
     {
-
-        public static List<int> lottozahlen = new List<int>();
-
         static void Main(string[] args)
         {
-
             Console.WriteLine("Zeit für Lotto! :");
 
-            InitializeBasket(49);
-
-            List<int> ziehung = Lotto6aus49();
-
+            Lotto lotto6aus49 = new Lotto(6, 49);
+            List<int> ziehung = lotto6aus49.GezogeneZahlen;
             AusgabeDerZiehung(ziehung);
 
             Console.WriteLine("Das Drücken einer beliebigen Taste beendet das Programm");
@@ -31,9 +22,7 @@ namespace Aufgabe_04
         private static void AusgabeDerZiehung(List<int> ziehung)
         {
             ziehung.Sort();
-
             StringBuilder sb = new StringBuilder();
-
 
             foreach (var zahjl in ziehung)
             {
@@ -41,83 +30,7 @@ namespace Aufgabe_04
             }
 
             string ergebis = sb.ToString();
-
-
             Console.WriteLine("Gezogen wurden die Zahlen : " + ergebis);
         }
-
-        private static void InitializeBasket(int anzahlZahlen)
-        {
-            List<int> lottozahlen1 = ShuffleBevore(anzahlZahlen);
-
-            for (int i = 0; i < anzahlZahlen; i++)
-            {
-                lottozahlen.Add(lottozahlen1[i]);
-            }
-        }
-
-        private static List<int> ShuffleBevore(int anzahlZahlen)
-        {
-            List<int> lottozahlen1 = new List<int>();
-
-            for (int i = 1; i <= anzahlZahlen; i++)
-            {
-                lottozahlen1.Add(i);
-            }
-
-
-            lottozahlen1 = Shuffle(lottozahlen1);
-            return lottozahlen1;
-        }
-
-        private static List<int> Lotto6aus49()
-        {
-            List<int> interimswerte = new List<int>();
-
-
-
-            for (int i = 0; i < 6; i++)
-            {
-                interimswerte = Einzelziehung(interimswerte);
-            }
-
-            return interimswerte;
-        }
-
-        private static List<int> Einzelziehung(List<int> interimswerte)
-        {
-            Random zufall = new Random(DateTime.Now.Millisecond);
-            int zufallszahl = zufall.Next(1, lottozahlen.Count);
-            interimswerte.Add(lottozahlen[zufallszahl - 1]);
-            lottozahlen.RemoveAt(zufallszahl - 1);
-            return interimswerte;
-        }
-
-
-
-
-        // Fisher-Yates shuffle:  see https://stackoverflow.com/questions/273313/randomize-a-listt
-        public static List<int> Shuffle(List<int> list)
-        {
-            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
-            int n = list.Count;
-            while (n > 1)
-            {
-                byte[] box = new byte[1];
-                do provider.GetBytes(box);
-                while (!(box[0] < n * (Byte.MaxValue / n)));
-                int k = (box[0] % n);
-                n--;
-                int value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-            return list;
-        }
     }
-
-
-
-
-
 }
